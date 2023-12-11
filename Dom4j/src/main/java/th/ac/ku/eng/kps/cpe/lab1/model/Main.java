2461e477ae4460ebda6dd68583559f7f4c0e5ac1
mon2384 <bar2384@hotmail.com>
2023-12-11 21:48:41 +0700

    ok test sed la

--- Dom4j/src/main/java/th/ac/ku/eng/kps/cpe/lab1/model/Main.java
+++ Dom4j/src/main/java/th/ac/ku/eng/kps/cpe/lab1/model/Main.java
@@ -1,7 +1,15 @@
 package th.ac.ku.eng.kps.cpe.lab1.model;
 
+import java.io.FileOutputStream;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentFactory;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		BookStore b = new BookStore() ;
 		b.setIsbn("0123456001");
@@ -12,6 +20,38 @@ public class Main {
 		b.setEdition((byte)7);
 		b.setPrice(19.99);
 
+		Document doc = DocumentFactory.getInstance().createDocument();
+		
+		Element bookstore = doc.addElement("bookstore");
+		
+		Element book = bookstore.addElement("book");
+		book.addAttribute("ISBN", b.getIsbn());
+
+		Element elem = book.addElement("title");
+		elem.addText(b.getTitle());
+		
+		elem = book.addElement("author");
+		elem.addText(b.getAuthor());
+		
+		elem = book.addElement("catagory");
+		elem.addText(b.getCatagory());
+		
+		elem = book.addElement("year");
+		elem.addText(Integer.toString(b.getYear()));
+		
+		elem = book.addElement("edition");
+		elem.addText(Byte.toString(b.getEdition()));
+		
+		elem = book.addElement("price");
+		elem.addText(Double.toString(b.getPrice()));
+
+		FileOutputStream fos = new FileOutputStream("lab6W.xml");
+		OutputFormat format = OutputFormat.createPrettyPrint();
+		XMLWriter writer = new XMLWriter(fos, format);
+		writer.write(doc);
+		writer.flush();
+		writer.close();
+		fos.close();
 		
 	}
 }
