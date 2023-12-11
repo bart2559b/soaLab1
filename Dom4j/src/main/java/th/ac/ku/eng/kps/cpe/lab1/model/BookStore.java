e36d32ab1adc00a8d47a40aba9a94b34865974f8
mon2384 <bar2384@hotmail.com>
2023-12-11 21:27:26 +0700

    Add get

--- Dom4j/src/main/java/th/ac/ku/eng/kps/cpe/lab1/model/BookStore.java
+++ Dom4j/src/main/java/th/ac/ku/eng/kps/cpe/lab1/model/BookStore.java
@@ -22,30 +22,51 @@ public class BookStore {
 	public void setIsbn(String string) {
 		this.isbn = string;
 	}
+	public String getIsbn() {
+		return this.isbn;
+	}
 
 	public void setTitle(String string) {
 		this.title = string;
+	}
+	public String getTitle() {
+		return this.title;
 	}
 
 	public void setAuthor(String string) {
 		this.author = string;
 	}
+	public String getAuthor() {
+		return this.author;
+	}
 
 	public void setCatagory(String string) {
 		this.catagory = string;
+	}
+	public String getCatagory() {
+		return this.catagory;
 	}
 
 	public void setEdition(byte b) {
 		this.edition = b;
 	}
+	public Byte getEdition() {
+		return this.edition;
+	}
 
 	public void setYear(int i) {
 		this.year = i;
+	}
+	public int getYear() {
+		return this.year;
 	}
 
 	public void setPrice(double d) {
 		this.price = d;
 		
+	}
+	public double getPrice() {
+		return this.price;
 	}
 	
 }
